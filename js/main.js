6880e8e7e2397c62d12831d91eef115d1ecc82b3
typosbro <typosbro@proton.me>
2023-08-18 12:05:32 +0900

    basic setup

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-window.addEventListener("load", () => {});
+import { $ } from "./utils.js";
+import { Bar } from "./Bar.js";
+
+window.addEventListener("load", () => {
+  const canvas = $("#canvas");
+  const ctx = canvas.getContext("2d");
+  const snail = $("#snail");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  const animate = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    requestAnimationFrame(animate);
+  };
+  animate();
+});
